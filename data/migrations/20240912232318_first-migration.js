47561617b72b4320650441e2f809465d1e539a8a
Azeem Brown <azeembrown717@gmail.com>
2024-09-15 22:51:30 -0400

    creating sql database

---
+++ data/migrations/20240912232318_first-migration.js
@@ -0,0 +1,39 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+    await knex.schema.createTable('projects', table => {
+        table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.string('project_description')
+        table.boolean('project_completed').defaultTo(false)
+    })
+    await knex.schema.createTable('resources', table => {
+        table.increments('resource_id')
+        table.string('resource_name').notNullable().unique()
+        table.string('resource_description')
+    })
+    await knex.schema.createTable('tasks', table => {
+        table.increments('task_id')
+        table.string('task_description').notNullable()
+        table.string('task_notes')
+        table.boolean('table_completed')
+            .defaultTo(false)
+        table.integer('projects_id').notNullable()
+            .references('projects_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+    await knex.schema.dropTableIfExists('tasks')
+    await knex.schema.dropTableIfExists('resources')
+    await knex.schema.dropTableIfExists('projects')
+};
